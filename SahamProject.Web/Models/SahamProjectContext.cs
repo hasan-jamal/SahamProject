9fbf1059e3216d9badcd512314e85b09e9a29b86
Hasan Abu Sharekh <hasan0595488660@gmail.com>
2022-12-24 00:19:43 +0200

    Add Editing Service

--- SahamProject.Web/Models/SahamProjectContext.cs
+++ SahamProject.Web/Models/SahamProjectContext.cs
@@ -28,6 +28,7 @@ namespace SahamProject.Web.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS;Database=SahamProject;Trusted_Connection=True;");
             }
         }
@@ -93,11 +94,7 @@ namespace SahamProject.Web.Models
 
             modelBuilder.Entity<Service>(entity =>
             {
-                entity.HasNoKey();
-
                 entity.Property(e => e.Description).HasMaxLength(1000);
-
-                entity.Property(e => e.Id).HasMaxLength(50);
 
                 entity.Property(e => e.ImageUrl).HasMaxLength(100);
 
--- SahamProject.Web/Models/Service.cs
+++ SahamProject.Web/Models/Service.cs
@@ -5,7 +5,7 @@ namespace SahamProject.Web.Models
 {
     public partial class Service
     {
-        public string Id { get; set; } = null!;
+        public int Id { get; set; }
         public string? ImageUrl { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
