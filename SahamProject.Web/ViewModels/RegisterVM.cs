4579f2b0f65e54aeef204bd60e7b91265373e08e
AhmadIbrahim99 <43531161+AhmadIbrahim99@users.noreply.github.com>
2022-12-24 04:13:57 +0200

    register view model

---
+++ SahamProject.Web/ViewModels/RegisterVM.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace SahamProject.Web.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Full name")]
+        [Required(ErrorMessage = "FullName is required")]
+        public string FullName { get; set; } = null!;
+
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "EmailAddress is required")]
+        public string EmailAddress { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Display(Name = "Confirm password")]
+        [Required(ErrorMessage = "ConfirmPassword is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not Match")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
