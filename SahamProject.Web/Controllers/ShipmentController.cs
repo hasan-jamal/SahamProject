50c09f27bc1783924f8fe7cf9687789fcaecfea1
AhmadIbrahim99 <43531161+AhmadIbrahim99@users.noreply.github.com>
2022-12-24 17:19:36 +0200

    add shipment MerchanId from controller

--- SahamProject.Web/Controllers/ShipmentController.cs
+++ SahamProject.Web/Controllers/ShipmentController.cs
@@ -1,15 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using SahamProject.Web.DataAccess.IRepository;
 using SahamProject.Web.Models;
+using System.Security.Claims;
 
 namespace SahamProject.Web.Controllers
 {
     public class ShipmentController : Controller
     {
         private readonly IUnitOfWork _unit;
-        public ShipmentController(IUnitOfWork unit)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public ShipmentController(IUnitOfWork unit, UserManager<ApplicationUser> userManager)
         {
             _unit = unit;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -23,7 +27,10 @@ namespace SahamProject.Web.Controllers
         [HttpPost]
         public IActionResult Update(Shipment shipment) 
         {
-            if(!ModelState.IsValid) return View(shipment);
+            
+            
+            if (!ModelState.IsValid || shipment.MerchanId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+                return View(shipment);
             _unit.shipments.Update(shipment);
             _unit.Save();
             return RedirectToAction(nameof(Index));
@@ -36,6 +43,7 @@ namespace SahamProject.Web.Controllers
         [HttpPost]
         public IActionResult Create(Shipment shipment)
         {
+            shipment.MerchanId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             if (!ModelState.IsValid) return View(shipment);
             _unit.shipments.Add(shipment);
             _unit.Save();
