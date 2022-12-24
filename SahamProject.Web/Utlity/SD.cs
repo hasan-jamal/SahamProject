9f7f62d374bb0cd257965142563ad515651807e2
AhmadIbrahim99 <43531161+AhmadIbrahim99@users.noreply.github.com>
2022-12-24 04:13:30 +0200

    User Roles

--- SahamProject.Web/Utlity/SD.cs
+++ SahamProject.Web/Utlity/SD.cs
@@ -4,6 +4,10 @@
     public static class SD
     {
         public const string Role_Admin = "Admin";
+        public const string Role_Admin_NormalizedName = "ADMIN";
         public const string Role_Merchant = "Merchant";
+        public const string Role_Merchant_NormalizedName = "MERCHANT";
+        public const string Role_Customer = "Customer ";
+        public const string Role_Customer_NormalizedName = "CUSTOMER";
     }
 }
