1e72c384e3c30cd2c7b67f68db612262c97e29f6
Hasan Abu Sharekh <hasan0595488660@gmail.com>
2023-01-30 13:43:27 +0200

    Update Roles

---
+++ SahamProject.Web/Migrations/20230130113946_Update_Roles.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SahamProject.Web.Migrations
+{
+    public partial class Update_Roles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "043114d0-4fb6-4f25-ac30-e0a1b32ddfbf");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "31ce0219-ccea-44e7-b714-ed5b6a20e2b9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "49683740-9f88-43d3-9083-49370dee79ef");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Width",
+                table: "ShipmentsProducts",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Weight",
+                table: "ShipmentsProducts",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Length",
+                table: "ShipmentsProducts",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Height",
+                table: "ShipmentsProducts",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "67da6857-770f-4bb7-89b6-ea74d068522c", "b964f375-6c2a-480f-a4c6-d3df5dbaf8bf", "Merchant", "MERCHANT" },
+                    { "a5761da7-a17f-48fa-bcd3-aa25d7b75d15", "0bc1cca7-2639-48db-a5f6-c6170c127a84", "Customer ", "CUSTOMER" },
+                    { "c3a2cde1-caac-4a6c-a24f-1b5d35b47f59", "71a00493-b5ea-46da-8d57-89686a308847", "Admin", "ADMIN" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "Area", "BuildingNumber", "City", "ConcurrencyStamp", "Country", "Email", "EmailConfirmed", "IsActive", "Location", "LocationMap", "LockoutEnabled", "LockoutEnd", "Name", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "ShortAddress", "StoreNo", "StreetName1", "StreetName2", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "a18be9c0-aa65-4af8-bd17-00bd4354e575", 0, null, null, null, "9fc10f5f-abb1-4894-b6bd-1f26594207a1", null, "customer@saham.com", true, true, null, null, false, null, "Customer", "customer@saham.com", "customer", "AQAAAAEAACcQAAAAEJ8tn5goWtbknKgYHhI0hqkHm9jUyCOjHnAZy8NxSU8WWtNcWH3heoomYqDOFLdFrg==", null, false, "", null, null, null, null, false, "customer" },
+                    { "a18be9c0-aa65-4af8-bd17-00bd9344e575", 0, null, null, null, "b39a2313-a7d8-490e-a83f-b53eea96c667", null, "admin@saham.com", true, true, null, null, false, null, "Admin", "admin@saham.com", "adminNew", "AQAAAAEAACcQAAAAEAnoeAFdYM+pRmXZ/xnJd+eEBvs7mWJQbNrjRSwGkrntgYb4OWp5JDFt33hFHTMvJA==", null, false, "", null, null, null, null, false, "adminNew" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "a5761da7-a17f-48fa-bcd3-aa25d7b75d15", "a18be9c0-aa65-4af8-bd17-00bd4354e575" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "c3a2cde1-caac-4a6c-a24f-1b5d35b47f59", "a18be9c0-aa65-4af8-bd17-00bd9344e575" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "67da6857-770f-4bb7-89b6-ea74d068522c");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "a5761da7-a17f-48fa-bcd3-aa25d7b75d15", "a18be9c0-aa65-4af8-bd17-00bd4354e575" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "c3a2cde1-caac-4a6c-a24f-1b5d35b47f59", "a18be9c0-aa65-4af8-bd17-00bd9344e575" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a5761da7-a17f-48fa-bcd3-aa25d7b75d15");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c3a2cde1-caac-4a6c-a24f-1b5d35b47f59");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd4354e575");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Width",
+                table: "ShipmentsProducts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Weight",
+                table: "ShipmentsProducts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Length",
+                table: "ShipmentsProducts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Height",
+                table: "ShipmentsProducts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "043114d0-4fb6-4f25-ac30-e0a1b32ddfbf", "54cfe117-ba81-4483-86d2-ba2d08d54107", "Merchant", "MERCHANT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "31ce0219-ccea-44e7-b714-ed5b6a20e2b9", "a43ba216-a58c-42de-ad9a-13ac9e2e82f0", "Customer ", "CUSTOMER" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "49683740-9f88-43d3-9083-49370dee79ef", "75932473-6b49-4247-b010-8a4fec0da355", "Admin", "ADMIN" });
+        }
+    }
+}
--- SahamProject.Web/Models/SahamProjectContext.cs
+++ SahamProject.Web/Models/SahamProjectContext.cs
@@ -25,9 +25,16 @@ namespace SahamProject.Web.Models
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+
+            const string ROLE_ID = "c3a2cde1-caac-4a6c-a24f-1b5d35b47f59";
+            const string ADMIN_ID = "a18be9c0-aa65-4af8-bd17-00bd9344e575";
+            const string CUSTOMER_ID = "a18be9c0-aa65-4af8-bd17-00bd4354e575";
+            const string ROLE_CUSTOMER_ID = "a5761da7-a17f-48fa-bcd3-aa25d7b75d15";
             builder.Entity<IdentityRole>().
                HasData(new IdentityRole
                {
+                   Id = ROLE_ID,
                    Name = SD.Role_Admin,
                    NormalizedName = SD.Role_Admin_NormalizedName
                });
@@ -35,15 +42,52 @@ namespace SahamProject.Web.Models
             builder.Entity<IdentityRole>().
                 HasData(new IdentityRole
                 {
+                    Id = ROLE_CUSTOMER_ID,
+                    Name = SD.Role_Customer,
+                    NormalizedName = SD.Role_Customer_NormalizedName
+                });
+            builder.Entity<IdentityRole>().
+                HasData(new IdentityRole
+                {
                     Name = SD.Role_Merchant,
                     NormalizedName = SD.Role_Merchant_NormalizedName
                 });
 
-            builder.Entity<IdentityRole>().
-                HasData(new IdentityRole
+            var hasher = new PasswordHasher<ApplicationUser>();
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = ADMIN_ID,
+                Name = "Admin",
+                UserName = "adminNew",
+                NormalizedUserName = "adminNew",
+                Email = "admin@saham.com",
+                NormalizedEmail = "admin@saham.com",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Admin@123#"),
+                SecurityStamp = string.Empty
+            });
+            builder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = CUSTOMER_ID,
+                Name = "Customer",
+                UserName = "customer",
+                NormalizedUserName = "customer",
+                Email = "customer@saham.com",
+                NormalizedEmail = "customer@saham.com",
+                EmailConfirmed = true,
+                PasswordHash = hasher.HashPassword(null, "Customer@123#"),
+                SecurityStamp = string.Empty
+            });
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
                 {
-                    Name = SD.Role_Customer,
-                    NormalizedName = SD.Role_Customer_NormalizedName
+                    RoleId = ROLE_ID,
+                    UserId = ADMIN_ID
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = ROLE_CUSTOMER_ID,
+                    UserId = CUSTOMER_ID
                 });
 
             builder.Entity<Shipment>().
@@ -62,7 +106,6 @@ namespace SahamProject.Web.Models
             builder.Entity<Status>().HasData(new Status { Id = 2, Name = "InPrgoress" });
             builder.Entity<Status>().HasData(new Status { Id = 3, Name = "Done" });
 
-            base.OnModelCreating(builder);
         }
 
 
